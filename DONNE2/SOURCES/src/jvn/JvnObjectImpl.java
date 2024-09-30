d5c5667cf7cc5789f16757b5baa0589db88593e2
Mathieu Zussy <mathieu.zussy@etu.univ-grenoble-alpes.fr>
2024-09-30 09:29:13 +0200

    JvnObj completed

--- DONNE2/SOURCES/src/jvn/JvnObjectImpl.java
+++ DONNE2/SOURCES/src/jvn/JvnObjectImpl.java
@@ -2,48 +2,57 @@ package jvn;
 
 import java.io.Serializable;
 
-public class JvnObjectImpl implements JvnObject{
-	
-	private static final long serialVersionUID = 1L;
+public class JvnObjectImpl implements JvnObject {
 
-	public JvnObjectImpl(Serializable o, JvnLocalServer js) {
-		
+	private static final long serialVersionUID = 1L;
+	
+	private int uid;
+	
+	private Serializable objValue;
+
+	private LockStates lockState;
+	
+	private JvnLocalServer localServer;
+
+	public JvnObjectImpl(Serializable o, JvnLocalServer js, Integer uid) {
+		this.objValue = o;
+		this.localServer = js;
+		this.lockState = LockStates.NL;
+		this.uid = uid;
 	}
 
 	@Override
 	public void jvnLockRead() throws JvnException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void jvnLockWrite() throws JvnException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void jvnUnLock() throws JvnException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public int jvnGetObjectId() throws JvnException {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.uid;
 	}
 
 	@Override
 	public Serializable jvnGetSharedObject() throws JvnException {
-		// TODO Auto-generated method stub
-		return null;
+		return objValue;
 	}
 
 	@Override
 	public void jvnInvalidateReader() throws JvnException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
