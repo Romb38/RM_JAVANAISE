28fe87818a5579d9e350c1ebc157541567549f60
m2gi-zussym <mathieu.zussy@etu.univ-grenoble-alpes.fr>
2024-10-14 09:26:47 +0200

    Proxy goes beep boop

--- DONNE2/SOURCES/src/jvn/ProxyImpl.java
+++ DONNE2/SOURCES/src/jvn/ProxyImpl.java
@@ -12,31 +12,46 @@ public class ProxyImpl implements InvocationHandler, Serializable{
 	public ProxyImpl(JvnObject obj) {
 		this.obj = obj;
 	}
+
+	public ProxyImpl(Serializable object, String instanceName) throws Exception {
+		JvnServerImpl server = JvnServerImpl.jvnGetServer();
+		this.obj = server.jvnLookupObject(instanceName);
+		
+		// Object doesn't exist, therefore it will be created
+		if(this.obj == null) {
+			this.obj = server.jvnCreateObject(object);
+			if(this.obj == null) {
+				throw new JvnException("Unable to create object");
+			}
+			this.obj.jvnUnLock();
+			server.jvnRegisterObject(instanceName, this.obj);
+		}
+	}
 	
-	public static Object newInstance(JvnObject obj) {
+	public static Object newInstance(Serializable obj, String instanceName) throws Exception {
 		return java.lang.reflect.Proxy.newProxyInstance(
 				obj.getClass().getClassLoader(),
 				obj.getClass().getInterfaces(),
-				new ProxyImpl(obj));
+				new ProxyImpl(obj, instanceName));
 	}
 
 	@Override
-	public Object invoke(Object arg0, Method arg1, Object[] arg2) throws Throwable {
+	public Object invoke(Object proxy, Method meth, Object[] arguments) throws Throwable {
 		Object result = null;
 		try {
-			if (arg1.isAnnotationPresent(MyAnotation.class)) {
-				switch (arg1.getAnnotation(MyAnotation.class).name()) {
+			if (meth.isAnnotationPresent(MyAnotation.class)) {
+				switch (meth.getAnnotation(MyAnotation.class).name()) {
 				case "read":
-					obj.jvnLockRead();
+					this.obj.jvnLockRead();
 					break;
 				case "write":
-					obj.jvnLockWrite();
+					this.obj.jvnLockWrite();
 					break;
 				default:
 				}
 			}
-			result = arg1.invoke(obj.jvnGetSharedObject(), arg2);
-			obj.jvnUnLock();
+			result = meth.invoke(this.obj.jvnGetSharedObject(), arguments);
+			this.obj.jvnUnLock();
 		} catch (Exception e) {
 		}
 		return result;
